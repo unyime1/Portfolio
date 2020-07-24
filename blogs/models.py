a6785ba14b7cea8a3ebb58381333a81153d95e68
Unyime Etim <lordunyime@gmail.com>
2020-07-24 05:11:22 +0100

    finished models def

--- blogs/models.py
+++ blogs/models.py
@@ -18,11 +18,10 @@ class Post(models.Model):
     featured_image = models.ImageField(null=True, blank=True, upload_to='images/')
     content = models.CharField(max_length=20000000, null=True, blank=True)
     slug = models.SlugField(max_length=600, null=True, blank=True, unique=True)
-    tag = models.ManyToManyField(Tag)
+    tag = models.ManyToManyField(Tag, null=True, blank=True,)
     
     def __str__(self):
         return self.title
-
 
 
 class Comment(models.Model):
