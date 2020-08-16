1e226d64e3b1b42e3eb4fcb82852692f463302bb
Unyime Etim <lordunyime@gmail.com>
2020-08-16 23:11:38 +0100

    ready to deploy

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import sys
+from pathlib import Path
 from .secrets import *
 from django.contrib.messages import constants as messages
+from google.oauth2 import service_account
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -92,7 +95,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'portfolio.wsgi.application'
 
-"""
+
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -132,7 +135,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -179,23 +182,24 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images') #defines profile picture location
 
+here = os.path.dirname(os.path.abspath(__file__))
 
-#DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
-#GS_BUCKET_NAME = 'my_new_portfolio_bucket'
-#STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
-#STATIC_URL = 'https://storage.googleapis.com/my_new_portfolio_bucket/'
-#GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
-#    'C:\Users\username\Downloads\my portfolio-a614a4394b44.json' # see step 3
-#)
+DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+GS_BUCKET_NAME = 'my_new_portfolio_bucket'
+STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+STATIC_URL = 'https://storage.googleapis.com/my_new_portfolio_bucket/'
+GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
+    os.path.join(here, 'portfolio-a614a4394b44.json') # see step 3
+)
 
 
 #S3 BUCKETS CONFIG
-AWS_ACCESS_KEY_ID =  AWS_ACCESS_KEY_ID   
-AWS_SECRET_ACCESS_KEY =  AWS_SECRET_ACCESS_KEY
-AWS_STORAGE_BUCKET_NAME = AWS_STORAGE_BUCKET_NAME  
-AWS_S3_FILE_OVERWRITE = False 
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#AWS_ACCESS_KEY_ID =  AWS_ACCESS_KEY_ID   
+#AWS_SECRET_ACCESS_KEY =  AWS_SECRET_ACCESS_KEY
+#AWS_STORAGE_BUCKET_NAME = AWS_STORAGE_BUCKET_NAME  
+#AWS_S3_FILE_OVERWRITE = False 
+#AWS_DEFAULT_ACL = None
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
